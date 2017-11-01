352c5d796e8ea017d205a55510336ae869489f4c
Sam Ewdala <same7.hamada@gmail.com>
2017-11-01 09:57:05 +0300

    moving to basic rules again

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import 'babel-polyfill';
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-import Routes from './routes';
-import './styles/styles.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
-render(<Routes />, document.querySelector('#app'));
-
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
--- src/index.test.js
+++
@@ -1,7 +0,0 @@
-import expect from 'expect';
-
-describe('Our First Test'), () => {
-    it('should pass', () => {
-        expect(true).toEqual(true);
-    });
-};
