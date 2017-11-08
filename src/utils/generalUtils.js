16c23a40eac66a1c273b415107adbd3a4bba8516
Sam Ewdala <same7.hamada@gmail.com>
2017-11-08 20:23:42 +0300

    starting moving the app to firebase
    instead of mock api

---
+++ src/utils/generalUtils.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+	fireBaseConfig: {
+		apiKey: 'AIzaSyBGb0Wk43w2uzM_QJCZiVNACZ01_oxwK00',
+		authDomain: 'react-redux-tdd.firebaseapp.com',
+		databaseURL: 'https://react-redux-tdd.firebaseio.com',
+		projectId: 'react-redux-tdd',
+		storageBucket: '',
+		messagingSenderId: '671846420991'
+	},
+};
